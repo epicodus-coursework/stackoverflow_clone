b47885051f8220cb9b1b1cc9217eb3d0cedb37a4
Matthew Terrill and Arsi <meterrill@gmail.com>
2015-03-23 14:37:37 -0700

    Add user registration and log in.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ActiveRecord::Base
   validates :email, :presence => true
   has_many :questions
   has_many :answers
+
+  def self.authenticate(email)
+    user = User.where(email: email).first
+    if user
+      user
+    else
+      nil
+    end
+  end
+
 end
